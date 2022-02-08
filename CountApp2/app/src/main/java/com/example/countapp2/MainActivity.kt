d3839612c30bf2944b928b5fefc41fb7066c1232
ShotaYokoyama13 <shotayokoyama2@gmail.com>
2022-02-08 23:21:20 +0900

    countapp2

---
+++ CountApp2/app/src/main/java/com/example/countapp2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.countapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberText: TextView = findViewById(R.id.numberText)
+        val plusButton: Button = findViewById(R.id.plusButton)
+        val minusButton: Button = findViewById(R.id.minusButton)
+        val clearButton: Button = findViewById(R.id.clearButton)
+
+        var num: Int = 0
+
+        plusButton.setOnClickListener{
+            num += 1
+            numberText.text = num.toString()
+        }
+        minusButton.setOnClickListener{
+            num -= 1
+            numberText.text = num.toString()
+        }
+        clearButton.setOnClickListener{
+            num = 0
+            numberText.text = num.toString()
+        }
+    }
+}
